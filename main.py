14ccbcb3a26893c6b3d2bf448b357eb425df060e
Ilham Rachmat <54584081+ilham-mmr@users.noreply.github.com>
2021-12-30 12:27:41 +0700

    remove load env

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from dotenv import load_dotenv
-import os
 from functions import run_flow
 
 if __name__ == '__main__':
-    load_dotenv(override=True)
-    username = os.getenv('USERNAME')
-    password = os.getenv('PASSWORD')
-    run_flow(username,password,'attendance.csv','ContentPlaceHolder1_gw1_ImageButton20_4')
+    run_flow('your_uum_username','your_uum_password','attendance.csv','ContentPlaceHolder1_gw1_ImageButton20_4')
